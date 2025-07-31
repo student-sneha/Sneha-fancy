13e1ded93bab406b9c68e7f5f5c36557af69b227
Sneha <snehakhandelwal373@gmail.com>
2025-07-31 10:10:44 +0530

    update orders

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -18,7 +18,7 @@ const Order = ({ token }) => {
         { headers: { token } }
       );
       if (response.data.success) {
-        setOrders(response.data.orders.reverse);
+        setOrders(response.data.orders);
       } else {
         toast.error(response.data.message);
       }
